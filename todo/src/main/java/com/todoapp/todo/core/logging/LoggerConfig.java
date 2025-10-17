39ddf485d3f741c1cb6719afc15bf5834a0b0282
benji_xxsybi <ben@forster.one>
2025-10-17 14:34:47 +0200

    User + UserDetialsImpl added with Logger config also added

--- todo/src/main/java/com/todoapp/todo/core/logging/LoggerConfig.java
+++ todo/src/main/java/com/todoapp/todo/core/logging/LoggerConfig.java
@@ -1,4 +1,30 @@
 package com.todoapp.todo.core.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.MethodParameter;
+
+@Configuration
 public class LoggerConfig {
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Logger logger(InjectionPoint injectionPoint) {
+        Logger logger = null;
+        try {
+            MethodParameter methodParameter = injectionPoint.getMethodParameter();
+            if (methodParameter != null) {
+                logger = LoggerFactory.getLogger(methodParameter.getContainingClass());
+            }
+        }
+        catch (NullPointerException npe) {
+            return null;
+        }
+        return logger;
+    }
 }
--- todo/src/main/java/com/todoapp/todo/domain/user/User.java
+++ todo/src/main/java/com/todoapp/todo/domain/user/User.java
@@ -1,4 +1,106 @@
 package com.todoapp.todo.domain.user;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.todoapp.todo.core.generic.ExtendedEntity;
+import com.todoapp.todo.domain.role.Role;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
-public class User {
+import java.util.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User extends ExtendedEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns =
+    @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id",
+    referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(UUID id, String firstname, String lastName, String email, String password, Set<Role> role) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public User(UUID id, String email, String password) {
+        super(id);
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
